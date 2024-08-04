4099e480aaeebe008625934db79061168c705957
KODY JAY <kodysdevelopment@gmail.com>
2024-08-04 19:57:49 +0100

    update contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,8 @@
-import { useRef, useState } from "react"
-
 const Contact = () => {
 
-    const ref = useRef(null);
-    const [height, setHeight] = useState("0px");
-    const onLoad = () => {
-        setHeight(ref.current.contentWindow.document.body.scrollHeight + "px");
-    };
 
     return (
-        <iframe ref={ref} onLoad={onLoad} style={{padding: 0, margin: 0, border: 0, overflow: "auto"}} title="Contact Form" src="https://plugins.crisp.chat/urn:crisp.im:contact-form:0/contact/f6afb8ca-9c70-47a7-b15e-13f5174626b6" referrerPolicy="origin" sandbox="allow-forms allow-popups allow-scripts allow-same-origin" height={height} width="100%"></iframe>
+        <iframe style={{padding: 0, margin: 0}} title="Contact Form" src="https://plugins.crisp.chat/urn:crisp.im:contact-form:0/contact/f6afb8ca-9c70-47a7-b15e-13f5174626b6" referrerPolicy="origin" sandbox="allow-forms allow-popups allow-scripts allow-same-origin" height="620px" width="100%" frameBorder={0}></iframe>
     )
 }
 
