4ce22a667a3b49e679b8d4581b438c9a82ac1e39
KODY JAY <kodysdevelopment@gmail.com>
2024-08-05 16:53:12 +0100

    updated form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
                 <iframe title="Contact Form" src="https://plugins.crisp.chat/urn:crisp.im:contact-form:0/contact/f6afb8ca-9c70-47a7-b15e-13f5174626b6" referrerPolicy="origin" sandbox="allow-forms allow-popups allow-scripts allow-same-origin" height="620px" width="100%" frameBorder={0}></iframe>
             </div>
 
-            <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+            <form name="contact" method="POST" netlify onSubmit="submit">
                 <p>
                     <label>Your Name: <input type="text" name="name" /></label>
                 </p>
