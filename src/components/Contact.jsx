6f9e995fa33a1a23298f50a734332c740da2238a
KODY JAY <kodysdevelopment@gmail.com>
2024-08-05 16:47:08 +0100

    testing netlify forms

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,31 @@ const Contact = () => {
                 <h1>Got a question? Email us through our contact form!</h1>
                 <iframe title="Contact Form" src="https://plugins.crisp.chat/urn:crisp.im:contact-form:0/contact/f6afb8ca-9c70-47a7-b15e-13f5174626b6" referrerPolicy="origin" sandbox="allow-forms allow-popups allow-scripts allow-same-origin" height="620px" width="100%" frameBorder={0}></iframe>
             </div>
+
+            <form name="contact" method="POST" data-netlify="true">
+                <p>
+                    <label>Your Name: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                    </select></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+                </form>
+            
         </div>
+
+        
         
     )
 }
