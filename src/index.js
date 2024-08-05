c69f11d01f9cf88ddb89d98b2d704c0d44dd381a
KODY JAY <kodysdevelopment@gmail.com>
2024-08-05 17:05:32 +0100

    form update

--- src/index.js
+++ src/index.js
@@ -6,5 +6,23 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div className="content-wrapper">
         <App />
+
+        <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+                <input type="hidden" name="form-name" value="contact" />
+                <label>
+                    First Name:
+                    <input type="text" name="first-name" />
+                </label>
+                <label htmlFor="email">
+                    Your Email: 
+                    <input id="email" type="email" name="email" />
+                </label>
+                <label>
+                    Message: 
+                    <textarea name="message" />
+                </label>
+
+                <button type="submit">Send</button>
+            </form>
     </div>
 );
